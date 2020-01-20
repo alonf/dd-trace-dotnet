645292e6a3c9e3a03b142f69335bd83013d49c49
Alon <alonf@codevalue.net>
2020-01-20 19:12:18 +0200

    Update settings to start OzCode and Datadogs

--- test/Datadog.Trace.TestHelpers/ProfilerHelper.cs
+++ test/Datadog.Trace.TestHelpers/ProfilerHelper.cs
@@ -43,10 +43,25 @@ namespace Datadog.Trace.TestHelpers
             {
                 // .NET Core
                 startInfo = new ProcessStartInfo(dotNetCoreExecutable, $"{appPath} {arguments ?? string.Empty}");
-
+                startInfo.EnvironmentVariables["OzCode:Agent:ForceLoad"] = "true";
                 startInfo.EnvironmentVariables["CORECLR_ENABLE_PROFILING"] = "1";
-                startInfo.EnvironmentVariables["CORECLR_PROFILER"] = profilerClsid;
-                startInfo.EnvironmentVariables["CORECLR_PROFILER_PATH"] = profilerDllPath;
+                startInfo.EnvironmentVariables["CORECLR_PROFILER"] = "{3BF080FE-7DEB-4051-AEF1-BD3AB01F1883}";
+                startInfo.EnvironmentVariables["CORECLR_PROFILER_PATH"] = @"C:\dev\OzCode\agent\dotnet\src\SkyApm.ProfilerMultiplexer\x64\Debug\SkyApm.ProfilerMultiplexer.dll";
+                startInfo.EnvironmentVariables["CORECLR_PROFILER_HOME"] = @"C:\dev\OzCode\agent\dotnet\src\SkyApm.ClrProfiler.Trace\bin\Debug\netstandard2.0";
+                startInfo.EnvironmentVariables["OzCode:Agent:ClientAddress"] = "https://ozcodeclouddebuggerclient-staging.azurewebsites.net";
+                startInfo.EnvironmentVariables["OzCode:Agent:ServerAddress"] = "NoServerWriteToConsoleInstead";
+                // startInfo.EnvironmentVariables["CORECLR_PROFILERS"] = "Datadog;SkyAPM";
+                startInfo.EnvironmentVariables["CORECLR_PROFILERS"] = "Datadog";
+
+                startInfo.EnvironmentVariables["CORECLR_Datadog_ENABLE_PROFILING"] = "1";
+                startInfo.EnvironmentVariables["CORECLR_Datadog_PROFILER"] = profilerClsid;
+                startInfo.EnvironmentVariables["CORECLR_Datadog_PROFILER_PATH"] = profilerDllPath;
+                startInfo.EnvironmentVariables["CORECLR_Datadog_PRIORITY"] = "1.0";
+
+                startInfo.EnvironmentVariables["CORECLR_SkyAPM_ENABLE_PROFILING"] = "1";
+                startInfo.EnvironmentVariables["CORECLR_SkyAPM_PROFILER"] = "{09992526-3e32-4995-8d3d-a97c839393e1}";
+                startInfo.EnvironmentVariables["CORECLR_SkyAPM_PROFILER_PATH"] = @"C:\dev\OzCode\agent\dotnet\src\SkyApm.ClrProfiler\x64\Debug\SkyApm.ClrProfiler.dll";
+                startInfo.EnvironmentVariables["CORECLR_SkyAPM_PRIORITY"] = "2.0";
 
                 startInfo.EnvironmentVariables["DD_PROFILER_PROCESSES"] = dotNetCoreExecutable;
             }
@@ -55,9 +70,25 @@ namespace Datadog.Trace.TestHelpers
                 // .NET Framework
                 startInfo = new ProcessStartInfo(appPath, $"{arguments ?? string.Empty}");
 
+                startInfo.EnvironmentVariables["OzCode:Agent:ForceLoad"] = "true";
                 startInfo.EnvironmentVariables["COR_ENABLE_PROFILING"] = "1";
-                startInfo.EnvironmentVariables["COR_PROFILER"] = profilerClsid;
-                startInfo.EnvironmentVariables["COR_PROFILER_PATH"] = profilerDllPath;
+                startInfo.EnvironmentVariables["COR_PROFILER"] = "{DBE6D54B-4A04-4FD0-83EB-12A1A9DEC58B}";
+                startInfo.EnvironmentVariables["COR_PROFILER_PATH"] = @"C:\dev\OzCode\agent\dotnet\src\SkyApm.ProfilerMultiplexer\x64\Debug\SkyApm.ProfilerMultiplexer.dll";
+                startInfo.EnvironmentVariables["COR_PROFILER_HOME"] = @"C:\dev\OzCode\agent\dotnet\src\SkyApm.ClrProfiler.Trace\bin\Debug\net461";
+                startInfo.EnvironmentVariables["OzCode:Agent:ClientAddress"] = "https://ozcodeclouddebuggerclient-staging.azurewebsites.net";
+                startInfo.EnvironmentVariables["OzCode:Agent:ServerAddress"] = "NoServerWriteToConsoleInstead";
+                startInfo.EnvironmentVariables["COR_PROFILERS"] = "Datadog;SkyAPM";
+                // startInfo.EnvironmentVariables["COR_PROFILERS"] = "Datadog";
+
+                startInfo.EnvironmentVariables["COR_Datadog_ENABLE_PROFILING"] = "1";
+                startInfo.EnvironmentVariables["COR_Datadog_PROFILER"] = profilerClsid;
+                startInfo.EnvironmentVariables["COR_Datadog_PROFILER_PATH"] = profilerDllPath;
+                startInfo.EnvironmentVariables["COR_Datadog_PRIORITY"] = "2.0";
+
+                startInfo.EnvironmentVariables["COR_SkyAPM_ENABLE_PROFILING"] = "1";
+                startInfo.EnvironmentVariables["COR_SkyAPM_PROFILER"] = "{af0d821e-299b-5307-a3d8-b283c03916dd}";
+                startInfo.EnvironmentVariables["COR_SkyAPM_PROFILER_PATH"] = @"C:\dev\OzCode\agent\dotnet\src\SkyApm.ClrProfiler\x64\Debug\SkyApm.ClrProfiler.dll";
+                startInfo.EnvironmentVariables["COR_SkyAPM_PRIORITY"] = "1.0";
 
                 string executableFileName = Path.GetFileName(appPath);
                 startInfo.EnvironmentVariables["DD_PROFILER_PROCESSES"] = executableFileName;
